34b38c213604b98038fe8c48cf1f87e658d39e4d
xiaoyong jiang <jiangxiaoyong904@gmail.com>
2016-06-27 22:17:28 -0400

    group messages per city

--- src/main/scala-2.11/com/streaming/analysis/StreamingAnalysis.scala
+++ src/main/scala-2.11/com/streaming/analysis/StreamingAnalysis.scala
@@ -14,6 +14,7 @@ class StreamingAnalysis {
   println("Initializing Streaming Spark Context...")
   val sparkConf = new SparkConf().setAppName("Twitter sentiment analysis").setMaster("local[*]")
   val sc = new StreamingContext(sparkConf, Seconds(5))
+  sc.checkpoint("/SparkCheckPoint") //needed for stateful transformation
 
   println("Initializing streaming parameters...")
   val props = new Properties()
@@ -31,11 +32,21 @@ class StreamingAnalysis {
     rawTweets.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/rawResults/tweet")
 
     val rawLocationText= rawTweets.map(TwitterObjectFactory.createStatus).map(CustomizeTweet(_))
-    rawLocationText.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/countryText/ct")
+    rawLocationText.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/rawLocationText/ct")
 
     //filtering tuple, sanitise location
-    val cityText = rawLocationText.filter(x => FilterUtils.sanitiseLocation(x._1)).map{ case (k,v) => (k.split(",")(0), v)}
-    cityText.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/cleanCityText/ct")
+    val cityText = rawLocationText.filter(x => TweetUtils.sanitiseLocation(x._1)).map{ case (k,v) => (k.split(",")(0), v)} //location from tweet, "city, country"
+    cityText.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/cityText/ct")
+
+    //filtering tuple, sanitise text
+    val cleanCityText =  cityText.map{ case (k, v) => (k, TweetUtils.filterOnlyWords(v))}.filter(x => TweetUtils.filterEmptyString(x._2))
+//    cleanCityText.print()
+    cleanCityText.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/cleanCityText/ct")
+
+    //group messages to their cities, accumulated from beginning of the app
+    val allMessagesPerCity = cleanCityText.updateStateByKey(TweetUtils.groupMessages _ )
+    allMessagesPerCity.print()
+    allMessagesPerCity.window(Seconds(5), Seconds(5)).saveAsTextFiles(s"/allMessagesPerCity/ct")
 
     // Start the streaming computation
     println("Spark Streaming begin...")
---
+++ src/main/scala-2.11/com/streaming/analysis/TweetUtils.scala
@@ -0,0 +1,31 @@
+package com.streaming.analysis
+
+object TweetUtils {
+  def sanitiseLocation(s: String): Boolean= {
+    filterUnknownLocation(s) && filterUnorganizedLocation(s)
+  }
+
+  def filterUnknownLocation(s: String): Boolean = {
+    if (s != "?") true
+    else false
+  }
+
+  def filterUnorganizedLocation(s: String): Boolean = {
+    if (s.contains(",")) true
+    else false
+  }
+
+  def filterOnlyWords(text: String): String = {
+    text.split(" ").filter(_.matches("^[a-zA-Z0-9 ]+$")).fold("")((a,b) => a + " " + b).trim
+  }
+
+  def filterEmptyString(s: String): Boolean = {
+    if ( s != "") true
+    else false
+  }
+
+  def groupMessages(currValues: Seq[String], prevValues: Option[String]) = {
+    Some(prevValues.getOrElse("") + currValues)
+  }
+
+}
